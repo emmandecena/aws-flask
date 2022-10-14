4c8ab1a54bd261ecffa815a0c7aecc21cf340036
emmandecena <ecdecena@gmail.com>
2022-10-14 13:36:41 +0800

    added templates

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
