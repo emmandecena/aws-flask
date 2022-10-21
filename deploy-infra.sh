4b6f54a7e4682c641edb1bd4bb98f2d9f24baf2d
emmandecena <ecdecena@gmail.com>
2022-10-21 19:07:51 +0800

    added static resources

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,8 +5,24 @@ REGION=us-east-1
 CLI_PROFILE=aws-flask
 EC2_INSTANCE_TYPE=t2.micro
 
-# Deploy the CloudFormation template
-echo -e "\n=========== Deploying main.yaml ===========" 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile aws-flask \
+    --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yaml ===========" 
+aws cloudformation deploy \
+--region $REGION \
+--profile $CLI_PROFILE \
+--stack-name $STACK_NAME-setup \
+--template-file setup.yaml \
+--no-fail-on-empty-changeset \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameter-overrides \
+CodePipelineBucket=$CODEPIPELINE_BUCKET 
+
+# Deploys the CloudFormation template
+echo -e "\n\n=========== Deploying main.yaml ===========" 
 aws cloudformation deploy \
 --region $REGION \
 --profile $CLI_PROFILE \
