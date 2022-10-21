70cc82a71db70855b990261cc64cc4bdb71f1487
emmandecena <ecdecena@gmail.com>
2022-10-21 19:08:25 +0800

    added profile route

--- hello.py
+++ hello.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 
 app = Flask(__name__,
             static_url_path='', 
             static_folder='templates')
 
 @app.route("/")
-@app.route("/index.html")
 def home():
     return render_template("index.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("pages-profile.html")
+
+
+@app.route("/context")
+def context():
+    user_agent = request.headers.get("User-Agent")
+    session_obj = session.values()
+    return list(session_obj)
 
